7c0fdd7ff0b2efea72db1ad6ee7f691be9a29e4b
GABBRIELJUA <20221040@uthh.edu.mx>
2026-03-11 17:22:43 -0600

    cambio a .onlines

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10699929
+    "domain": "5gvci.com",
+    "zoneId": 10716573
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
